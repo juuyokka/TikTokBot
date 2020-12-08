cd2baf27d390a8deee21b7a704a884c9840f5291
juuyokka <yokotaniemi@gmail.com>
2020-12-07 19:49:40 -0800

    📚 Added support for multiple links

--- src/modules/checkTikTok.ts
+++ src/modules/checkTikTok.ts
@@ -1,11 +1,11 @@
-const tikTokRegex = /^https?:\/\/(.+\.)*tiktok.com\/\S+/
+const tikTokRegex = /https?:\/\/(.+\.)*tiktok.com\/\S+/g
 
 /**
- * Finds the first TikTok link from a string
+ * Finds all TikTok links from a string
  * 
  * @param s The string to process
- * @returns The first occurance of a TikTok link or `null` if one was not found
+ * @returns An array of the matches
  */
-export default (s: string): string | null => {
-    return s.match(tikTokRegex)?.[0] ?? null;
+export default (s: string): RegExpMatchArray | null => {
+    return s.match(tikTokRegex);
 }
